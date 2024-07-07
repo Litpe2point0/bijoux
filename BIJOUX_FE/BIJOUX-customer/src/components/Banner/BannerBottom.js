a37fb85ffe8fcbdab5f236d43946d81aeb8772c5
MeanLock <soryzzmikely@gmail.com>
2024-07-07 18:46:57 +0700

    update homepage

--- BIJOUX_FE/BIJOUX-customer/src/components/Banner/BannerBottom.js
+++ BIJOUX_FE/BIJOUX-customer/src/components/Banner/BannerBottom.js
@@ -6,7 +6,7 @@ const BannerBottom = () => {
   return (
     <div className="w-full bg-white border-b-[1px] py-4 border-b-gray-200 px-4">
       <div className="max-w-container mx-auto h-20 flex flex-col md:flex-row justify-between items-center">
-        <div className="flex items-center gap-2 w-72 shadow-sm hover:shadow-md duration-300">
+        <div className="flex md:w-auto items-center gap-2 w-72 shadow-sm hover:shadow-md duration-300">
           <span className="font-bold font-titleFont w-6 text-center">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -38,7 +38,7 @@ const BannerBottom = () => {
               </g>
             </svg>
           </span>
-          <p className="text-lightText text-base ml-5">Bảo Hành Trọn Đời</p>
+          <p className="text-lightText text-base ml-5">Life-time warranty</p>
         </div>
         <div className="flex md:w-auto items-center gap-2 w-72 shadow-sm hover:shadow-md duration-300">
           <span className="text-5xl text-center w-10 ml-1 text-orange-500">
